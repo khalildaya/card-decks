525ce5f9ba9adb94bb8cd81152f74708406c3f7a
Daya <212627323@logon.ds.ge.com>
2019-08-20 22:40:53 -0400

    adds reference to dealt cards

--- card-deck/index.js
+++ card-deck/index.js
@@ -13,7 +13,12 @@ function CardDeck(cardConfig) {
 	 * with the ones provided in constructor config
 	 */
 	const finalConfig = Object.assign(defaultConfig, cardConfig);
+
+	// build an ordered deck
 	this.deck = buildDeck(finalConfig.cardSuits, finalConfig.faceCards, finalConfig.cardNumbers);
+
+	// Initialize dealt cards
+	this.dealtCards = [];
 }
 
 
@@ -57,15 +62,57 @@ CardDeck.prototype.shuffle = function() {
 	 */
 	const tempDeck = [];
 
+	// Return all dealt cards back to deck and re-initiailze dealt cards
+	this.deck = this.deck.concat(this.dealtCards);
+	this.dealtCards = [];
+
 	while(this.deck.length > 0) {
 		// Pick a random card
 		const nextCardIndex = Math.floor(Math.random() * this.deck.length);
 		
 		// Remove the chosen card from the original deck and put it temp deck
-		const nextCard = this.deck.splice(nextCardIndex, 1);
+		const nextCard = this.deck.splice(nextCardIndex, 1)[0];
 		tempDeck.push(nextCard);
 	}
 	
 	// swap temp deck with original deck
 	this.deck = tempDeck;
+}
+
+/**
+ * Deals a card. Removes a card from the deck and adds
+ * it to the dealt cards
+ * @return {string} the dealt card
+*/
+CardDeck.prototype.dealCard = function() {
+	// Remove card from the deck
+	const card = this.deck.pop();
+
+	// Add card to dealt cards
+	this.dealtCards.push(card);
+
+	return card;
+}
+
+/**
+ * Deals all cards. Deck will be emptied.
+ * @return {array} array of string representing all dealt cards
+*/
+CardDeck.prototype.dealAllCards = function() {
+	// Array of dealt cards to return
+	const result = [];
+
+	/**
+	 * Loop through all cards in the deck and
+	 * pop them out of the deck until deck is empty.
+	 * Teh reason to return the array "result" instead of this.dealtCards
+	 * is to avoid returning a reference to an internal property of the CardDeck object
+	*/
+	while(this.deck.length > 0) {
+		const card = this.deck.pop(); 
+		result.push(card);
+		this.dealtCards.push(card);
+	}
+
+	return result;
 }
--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const CardDeck = require("./card-deck");
 
 c = new CardDeck();
 c.shuffle();
+for (let i = 0; i < 20; i++) {
+	c.dealCard();
+}
+console.log(c);
+c.shuffle();
+console.log(c);
+c.dealAllCards();
+console.log(c);
